7dddbf832021cfe3b99a1db7ae4fd0055534f55f
Krishna Yadav <yadavkrishnarajendra@gmail.com>
2025-05-26 17:14:37 +0530

    add number code

---
+++ Practice/src/PracticeQuestions/AddNumbers.java
@@ -0,0 +1,19 @@
+package PracticeQuestions;
+
+public class AddNumbers {
+
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 20;
+		
+		int addition = add(x,y);
+
+		System.out.println(addition);
+	}
+
+	private static int add(int x, int y) {
+		
+		return x+y;
+	}
+
+}
